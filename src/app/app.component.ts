8fd39025087235c839b0fb83e848508561b7e855
Felonius Gru <new-comet-dev@outlook.com>
2018-04-10 11:40:38 +0400

    save user email address when auto login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { UserProvider } from '../providers/user/user';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,7 +13,7 @@ export class MyApp {
   // rootPage: string = 'LoginToSoaringSafePage';
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
-    public afAuth: AngularFireAuth) {
+    public afAuth: AngularFireAuth, public userService: UserProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -22,6 +23,7 @@ export class MyApp {
       // Check If User is logged in or not
       this.afAuth.authState.subscribe(user=>{
         if(user) {
+          userService.currentUserEmail = user.email
           this.rootPage = 'TabsControllerPage';
         } else {
           this.rootPage = 'LoginToSoaringSafePage'

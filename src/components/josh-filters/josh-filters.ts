6d1c9d0595b5e44ff0eb2ea92bbd1d459fef2218
Ali Raza <razaminhas11@gmail.com>
2018-05-10 23:30:41 +0500

    New Fixes to the code

--- src/components/josh-filters/josh-filters.ts
+++ src/components/josh-filters/josh-filters.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 import { NavController, ModalController } from 'ionic-angular';
 import { FiltersProvider } from '../../providers/filters/filters';
 import { Storage } from '@ionic/storage';
-
+// import $ from "jquery";
+//var $=window.jQuery;
 @Component({
   selector: 'josh-filters',
   templateUrl: 'josh-filters.html'
